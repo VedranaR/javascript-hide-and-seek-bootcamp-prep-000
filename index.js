e7e6544cefce0fae9398297c5362bb5f46bd9ca7
Vedrana <vedrana_ravlic16@yahoo.com>
2018-02-06 20:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget(){
-    return document.getElementById("nested").querySelector(".target");
+  return document.getElementById("nested").querySelector(".target");
 }
 
 function increaseRankBy(n){
-  
-}
+  var liLi=document.getElementById("app").querySelectorAll("ul.ranked-list li");
+    for(let i=0; i<liLi.length; i++){
+      liLi[i].innerHTML=parseInt(lis[i].innerHTML)+n;
+}}
